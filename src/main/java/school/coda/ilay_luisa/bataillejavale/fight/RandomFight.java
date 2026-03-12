be4cd3be76b578f764630a9876f92b159e7df865
Vresca <ilay-selin.kocak@coda-student.school>
2026-03-12 09:34:03 +0100

    Changement du nom du fichier Board.java, Cat.java, Game.java, menu.fxml, MenuController.java, RandonFight.java
    Co-authored-by:Luisa <Luisa-hub12@users.noreply.github.com>

--- src/main/java/school/coda/ilay_luisa/bataillejavale/fight/RandomFight.java
+++ src/main/java/school/coda/ilay_luisa/bataillejavale/fight/RandomFight.java
@@ -5,7 +5,8 @@ import school.coda.ilay_luisa.bataillejavale.model.CoordinatePoints;
 public class RandomFight {
     private final java.util.Random random = new java.util.Random();
 
-    public CoordinatePoints chooseAttack() {
+    public CoordinatePoints chooseAttack()
+    {
         int row = random.nextInt(10);
         int col = random.nextInt(10);
 
